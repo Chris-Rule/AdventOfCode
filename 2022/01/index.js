6a06dbc53b6cb235c13564a2606f36f6e0a3e4e4
Chris <38883071+Chris-Rule@users.noreply.github.com>
2022-12-04 18:27:29 +0000

    Day 1 part 1 complete

--- 2022/01/index.js
+++ 2022/01/index.js
@@ -1,7 +1,37 @@
-import fs from 'fs';
+var fs = require('fs');
 
-const input = fs.readFile('input.txt','utf8',function(err,data){
-    console.log(data);
+//read input
+const input = fs.readFileSync('input.txt','utf8',function(err,data){
+    console.log(typeof input);
     return data;
 })
 
+//format input
+const values = input.split("\r\n");
+
+let result = 0;
+let tally = 0;
+
+//find highest weight
+for(let i = 0; i<values.length; i++){
+    if(values[i] != ""){
+        tally += parseInt(values[i],10);
+    } else {
+        if(tally > result){
+            result = tally;
+        }
+        tally = 0;
+    }
+}
+
+console.log("result = ", result);
+
+
+
+
+
+
+
+//const splitData = input.split("/n");
+
+//console.log(splitData);
